96884f78b4884e5d94e15b4bfac40f9544048e3b
cheakf <sunmoming@gmail.com>
2024-01-23 12:46:00 +0800

    parse add

--- include/parse.hpp
+++ include/parse.hpp
@@ -8,29 +8,29 @@ namespace swq
 
     // 给json用于解析的类
     // 这部分仅在解析文件时使用，所以单独组成一个类，在分析完成后析构，不保留相关数据，提高速度
-    // class json_parse
-    // {
-    // public:
-    //     json_parse() = default;
-    //     ~json_parse() = default;
-    //     json_parse(const std::string &input_str);
+    class json_parse
+    {
+    public:
+        json_parse() = default;
+        ~json_parse() = default;
+        json_parse(const std::string &input_str);
 
-    //     void load(const std::string &input_str);
-    //     json parse();
+        void load(const std::string &input_str);
+        json parse();
 
-    // private:
-    //     char get_next_token();
-    //     bool in_range(int x, int lower, int upper);
+    private:
+        char get_next_token();
+        bool in_range(int x, int lower, int upper);
 
-    //     json parse_null();
-    //     json parse_bool();
-    //     json parse_number();
-    //     json parse_string();
-    //     json parse_array();
-    //     json parse_object();
+        json parse_null();
+        json parse_bool();
+        json parse_number();
+        json parse_string();
+        json parse_array();
+        json parse_object();
 
-    //     std::string m_str = "";
-    //     size_t m_idx = 0;
-    //     bool end_label = false;
-    // };
+        std::string m_str = "";
+        size_t m_idx = 0;
+        bool end_label = false;
+    };
 }
